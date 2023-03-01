c8cbbf157036f85fd0e81359a083dff9e1219309
Cuong Ly <cuonglyy@gmail.com>
2023-03-01 10:21:39 -0800

    Name of components appear through script

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const scoreNum = document.querySelector('.score-number');
 const itemIcon = document.querySelectorAll('.item__icon');
-const itemName = document.querySelectoryAll('.item__name');
+const itemName = document.querySelectorAll('.item__name');
 const itemScoreNum = document.querySelectorAll('.item__score-num');
 
-//fetch('./data.json')
+fetch('./data.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP Error status' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        //console.log(data);
+        data.forEach( (item, i) => {
+            itemName[i].textContent = item.category;
+        })
+    })
