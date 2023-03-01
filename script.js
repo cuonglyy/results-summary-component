d710a515f85df5546afb83aff056144688241e54
Cuong Ly <cuonglyy@gmail.com>
2023-03-01 08:02:23 -0800

    Setting up script

---
+++ script.js
@@ -0,0 +1,6 @@
+const scoreNum = document.querySelector('.score-number');
+const itemIcon = document.querySelectorAll('.item__icon');
+const itemName = document.querySelectoryAll('.item__name');
+const itemScoreNum = document.querySelectorAll('.item__score-num');
+
+//fetch('./data.json')
