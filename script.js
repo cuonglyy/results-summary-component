aae6c09c8ef1c48fd5d9b59b45dd94063359406a
Cuong Ly <cuonglyy@gmail.com>
2023-03-01 13:03:24 -0800

    Finished script

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const itemIcon = document.querySelectorAll('.item__icon');
 const itemName = document.querySelectorAll('.item__name');
 const itemScoreNum = document.querySelectorAll('.item__score-num');
 
+let totalScore = 0;
+const maxScore = 400;
+
 fetch('./data.json')
     .then(response => {
         if (!response.ok) {
@@ -13,6 +16,15 @@ fetch('./data.json')
     .then(data => {
         //console.log(data);
         data.forEach( (item, i) => {
+            const img = new Image();
+            img.src = item.icon
+            itemIcon[i].appendChild(img);
             itemName[i].textContent = item.category;
+            itemScoreNum[i].textContent = item.score;
+
+            totalScore += item.score;
         })
+        totalScore = Math.floor((totalScore / maxScore) * 100);
+        //console.log(totalScore);
+        scoreNum.textContent = totalScore;
     })
